a52540f82085cf140053a5cdb6c442ab6079c91d
david11yf29 <david11yf29@gmail.com>
2020-05-15 00:07:55 +0800

    updated

--- src/components/menu-container/menu-container.components.jsx
+++ src/components/menu-container/menu-container.components.jsx
@@ -4,6 +4,12 @@ import './menu-container.styles.scss';
 
 import MenuItem from '../menu-item/menu-item.components';
 
+import CHEST from '../../assets/pics/chest.jpg';
+import BACK from '../../assets/pics/back.jpg';
+import LEGS from '../../assets/pics/legs.jpg';
+import SHOULDER from '../../assets/pics/shoulder.jpg';
+import CORE from '../../assets/pics/core.jpg';
+
 class MenuContainer extends React.Component {
     constructor() {
         super();
@@ -12,32 +18,32 @@ class MenuContainer extends React.Component {
             sections: [
                 {
                   title: 'CHEST',
-                  imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
+                  image: CHEST,
                   id: 1,
                   linkUrl: 'shop/chest'
                 },
                 {
                   title: 'BACK',
-                  imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
+                  image: BACK,
                   id: 2,
                   linkUrl: 'shop/back'
                 },
                 {
                   title: 'LEGS',
-                  imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
+                  image: LEGS,
                   id: 3,
                   linkUrl: 'shop/legs'
                 },
                 {
                   title: 'SHOULDER',
-                  imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
+                  image: SHOULDER,
                   size: 'large',
                   id: 4,
                   linkUrl: 'shop/shoulder'
                 },
                 {
                   title: 'CORE',
-                  imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
+                  image: CORE,
                   size: 'large',
                   id: 5,
                   linkUrl: 'shop/core'
@@ -51,7 +57,12 @@ class MenuContainer extends React.Component {
             <div className="menu-container">
                 {
                     this.state.sections.map(section => {
-                        return <MenuItem id={section.id} title={section.title} />
+                        return (
+                            <MenuItem 
+                            id={section.id} 
+                            title={section.title} 
+                            image={section.image} />
+                        )
                     })
                 }
             </div>
