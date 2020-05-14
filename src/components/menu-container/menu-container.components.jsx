5905d49ac1c8aee459f644b4b8b7c92bb7efa494
david11yf29 <david11yf29@gmail.com>
2020-05-15 00:00:25 +0800

    updated

--- src/components/menu-container/menu-container.components.jsx
+++ src/components/menu-container/menu-container.components.jsx
@@ -11,36 +11,36 @@ class MenuContainer extends React.Component {
         this.state = {
             sections: [
                 {
-                  title: 'hats',
+                  title: 'CHEST',
                   imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
                   id: 1,
-                  linkUrl: 'shop/hats'
+                  linkUrl: 'shop/chest'
                 },
                 {
-                  title: 'jackets',
+                  title: 'BACK',
                   imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
                   id: 2,
-                  linkUrl: 'shop/jackets'
+                  linkUrl: 'shop/back'
                 },
                 {
-                  title: 'sneakers',
+                  title: 'LEGS',
                   imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
                   id: 3,
-                  linkUrl: 'shop/sneakers'
+                  linkUrl: 'shop/legs'
                 },
                 {
-                  title: 'womens',
+                  title: 'SHOULDER',
                   imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
                   size: 'large',
                   id: 4,
-                  linkUrl: 'shop/womens'
+                  linkUrl: 'shop/shoulder'
                 },
                 {
-                  title: 'mens',
+                  title: 'CORE',
                   imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
                   size: 'large',
                   id: 5,
-                  linkUrl: 'shop/mens'
+                  linkUrl: 'shop/core'
                 }
               ]
         }
@@ -49,11 +49,11 @@ class MenuContainer extends React.Component {
     render() {
         return (
             <div className="menu-container">
-                <MenuItem title="CHEST" />
-                <MenuItem title="BACK" />
-                <MenuItem title="LEGS" />
-                <MenuItem title="SHOULDER" />
-                <MenuItem title="CORE" />
+                {
+                    this.state.sections.map(section => {
+                        return <MenuItem id={section.id} title={section.title} />
+                    })
+                }
             </div>
         )
     }
