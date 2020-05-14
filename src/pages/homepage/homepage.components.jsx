e104b60056c23f7aa981e854f0b4a3743aca00f1
david11yf29 <david11yf29@gmail.com>
2020-05-14 23:48:56 +0800

    updated

--- src/pages/homepage/homepage.components.jsx
+++ src/pages/homepage/homepage.components.jsx
@@ -2,41 +2,12 @@ import React from 'react';
 
 import './homepage.styles.scss';
 
+import MenuContainer from '../../components/menu-container/menu-container.components';
+
 const HomePage = () => {
     return (
         <div className="homapage">
-            <div className="menu-container">
-                <div className="menu-item">
-                    <div className="content">
-                        <h1 className="title">CHEST</h1>
-                        <span className="subtitle">SHOP NOW</span>
-                    </div>
-                </div>
-                <div className="menu-item">
-                    <div className="content">
-                        <h1 className="title">BACK</h1>
-                        <span className="subtitle">SHOP NOW</span>
-                    </div>
-                </div>
-                <div className="menu-item">
-                    <div className="content">
-                        <h1 className="title">LEGS</h1>
-                        <span className="subtitle">SHOP NOW</span>
-                    </div>
-                </div>
-                <div className="menu-item">
-                    <div className="content">
-                        <h1 className="title">SHOULDER</h1>
-                        <span className="subtitle">SHOP NOW</span>
-                    </div>
-                </div>
-                <div className="menu-item">
-                    <div className="content">
-                        <h1 className="title">CORE</h1>
-                        <span className="subtitle">SHOP NOW</span>
-                    </div>
-                </div>
-            </div>
+            <MenuContainer />
         </div>
     )
 }
