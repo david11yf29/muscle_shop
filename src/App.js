1d04c5bef34c543f559739619cd1ea446135c829
david11yf29 <david11yf29@gmail.com>
2020-05-15 16:16:14 +0800

    updated

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.components';
+import ShopPage from './pages/shop/shop.components.jsx';
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/shop" component={ShopPage} />
+      </Switch>
     </div>
   );
 }
