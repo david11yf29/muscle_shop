48f267701f473b4c2ba84bdc494fd2a09e51c6a3
david11yf29 <david11yf29@gmail.com>
2020-05-19 15:23:16 +0800

    updated

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -11,7 +11,39 @@ const config = {
     messagingSenderId: "132806049140",
     appId: "1:132806049140:web:8a703da72cbf828dd04021",
     measurementId: "G-RYWTQE32DL"
+};
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+    if (!userAuth) return;
+
+    // 先用 userRef 抓那個位置地址 再用 snapShot 抓那個地址裡面的資料確認裡面有沒有東西(exist)
+    const userRef = firestore.doc(`/users/${userAuth.uid}`);
+
+    const snapShot = await userRef.get();
+
+    console.log('snapShot', snapShot);
+
+    // 如果地址裡面沒有資料的話
+    if (!snapShot.exists) {
+        const { displayName, email } = userAuth;
+        const createdAt = new Date();
+
+        try {
+            // 在這個地址裡面新增資料
+            userRef.set({
+                displayName,
+                email,
+                createdAt,
+                ...additionalData
+            })
+        } catch (e) {
+            console.log('error creating user', e.message)
+        }
+    }
+
+    return userRef;
 }
+
 
 firebase.initializeApp(config);
 
