6773d69895e98a2df2cb38018290903108ba714b
david11yf29 <david11yf29@gmail.com>
2020-05-20 20:40:35 +0800

    updated

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -4,6 +4,7 @@ import { connect } from 'react-redux';
 
 import { auth } from '../../firebase/firebase.utils.js';
 import CartIcon from '../cart-icon/cart-icon.component';
+import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 
 import { ReactComponent as Logo } from '../../assets/pics3/logo.svg';
 
@@ -30,6 +31,7 @@ const Header = (props) => {
                 }
                 <CartIcon />
             </div>
+            <CartDropdown />
         </div>
     )
 }
