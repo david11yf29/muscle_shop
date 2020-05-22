4631fc6343980f29fef29e01281a48c5a7342556
david11yf29 <david11yf29@gmail.com>
2020-05-22 16:56:17 +0800

    updated

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
 
 import { toggleCartHidden } from '../../redux/cart/cart.actions.js';
 import { selectCartItemsCount } from '../../redux/cart/cart.selectors.js';
@@ -23,11 +24,8 @@ const mapDispatchToProps = (dispatch) => {
     }
 }
 
-const mapStateToProps = (state) => {
-    console.log("cart-icon");
-    return {
-        itemCount: selectCartItemsCount(state)
-    }
-}
+const mapStateToProps = createStructuredSelector({
+    itemCount: selectCartItemsCount
+})
 
 export default connect(mapStateToProps, mapDispatchToProps)(CartIcon);
