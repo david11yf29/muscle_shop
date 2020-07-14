d1d1a66fb67de61f119b7397d3c3b9d430652e9f
david11yf29 <david11yf29@gmail.com>
2020-07-14 08:17:02 +0800

    updated

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -35,6 +35,11 @@ const CheckoutPage = (props) => {
                 })
             }
             <div className="total">TOTAL: ${props.total}</div>
+            <div className="test-warning">
+                *Please use the following test credit card for payments*
+                <br />
+                4242 4242 4242 4242 - Exp: 01/22 - CVV: 123
+            </div>
             <StripeCheckoutButton price={props.total} />
         </div> 
     )
