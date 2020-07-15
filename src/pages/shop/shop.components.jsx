48212d1ac499b08745b5a126059013d97497de54
david11yf29 <david11yf29@gmail.com>
2020-07-16 07:20:19 +0800

    updated

--- src/pages/shop/shop.components.jsx
+++ src/pages/shop/shop.components.jsx
@@ -1,20 +1,44 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
+import { connect } from 'react-redux';
 
 import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
 import CollectionPage from '../collection/collection.component';
 
-const ShopPage = ({ match }) => {
-    // console.log(match);
-    return (
-        <div className="shop-page">
-            <Route exact path={`${match.path}`} component={CollectionsOverview} />
-            <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
-        </div>
-    )
+import { firestore, convertCollectionsSnapshotToMap } from '../../firebase/firebase.utils.js';
+
+import { updateCollections } from '../../redux/shop/shop.actions.js';
+
+class ShopPage extends React.Component{
+    unsubscribeFromSnapshot = null;
+
+    componentDidMount() {
+        const { updateCollections } = this.props;
+        const collectionRef = firestore.collection('collections');
+        this.unsubscribeFromSnapshot = collectionRef.onSnapshot(async snapshot => {
+            const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
+            updateCollections(collectionsMap);
+        })
+    }
+
+    render() {
+        const { match } = this.props;
+        return (
+            <div className="shop-page">
+                <Route exact path={`${match.path}`} component={CollectionsOverview} />
+                <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
+            </div>
+        )
+    }
 }
 
-export default ShopPage;
+const mapDispatchToProps = (dispatch) => {
+    return {
+        updateCollections: collectionsMap => dispatch(updateCollections(collectionsMap))
+    }
+}
+
+export default connect(null, mapDispatchToProps)(ShopPage);
 
 // 0: {id: 1, title: "chest", routeName: "chest", items: Array(5)}
 // 1: {id: 2, title: "back", routeName: "back", items: Array(5)}
